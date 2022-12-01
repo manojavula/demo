82e278fed0dc4e5ed2dd417920bfbaa710aa3390
manoj <manoj@email.com>
2022-12-01 18:11:45 +0530

    For Test

---
+++ demo/src/main/java/com/example/demo/test/Test.java
@@ -0,0 +1,14 @@
+package com.example.demo.test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
